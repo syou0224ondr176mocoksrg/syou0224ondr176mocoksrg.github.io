789279608ddb38d79b5ee0633ee1fa9de690ae07
syou0224ondr176mocoksrg <33883752+syou0224ondr176mocoksrg@users.noreply.github.com>
2018-10-16 16:11:13 +0900

    ポートフォリオ

--- portfolio/js/fadein.js
+++ portfolio/js/fadein.js
@@ -1,7 +1,6 @@
-$('#nice').delay(800 ).fadeIn("slow");
+$('#nice').delay(200 ).fadeIn("slow");
         $('#nb').delay(200 ).fadeIn("slow");
         $('#menu').delay(200 ).fadeIn("slow");
-        $('body').delay(200).fadeIn("slow");
         $(function(){
   $('a.fadelink').on("click",function() {
     var url = $(this).attr('href');
